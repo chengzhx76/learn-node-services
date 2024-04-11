93655d3a4fde5a32cf5817cab6d1b5a80ae6a33f
chengzhx76 <chengzhx76@qq.com>
2024-04-11 20:40:14 +0800

    u

--- nest-start/src/app.controller.ts
+++ nest-start/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,22 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('api')
+  async api() {
+    return this.appService.PromiseApi();
+  }
+
+  @Get('get')
+  async request() {
+    const data = await this.appService.getApi();
+    return data;
+  }
+
+  @Post('post')
+  async post() {
+    const data = await this.appService.postApi();
+    return data;
   }
 }
--- nest-start/src/app.service.ts
+++ nest-start/src/app.service.ts
@@ -1,8 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  async PromiseApi(): Promise<any> {
+    return await new Promise(async (resolve) => {
+      await sleep(1000);
+      resolve('Hi');
+    });
+  }
+
+  async getApi(): Promise<any> {
+    try {
+      const response = await axios.get('https://chengzhx76.cn/pinche-test/ping');
+      await sleep(5000);
+      return response.data;
+    } catch (error) {
+      // 处理错误，例如记录日志或抛出异常
+      console.error('Error fetching external data:', error);
+      throw error;
+    }
+  }
+  
+  async postApi(): Promise<any> {
+    try {
+      const response = await axios.post('https://chengzhx76.cn/pinche-test/ping');
+      await sleep(5000);
+      return response.data;
+    } catch (error) {
+      // 处理错误，例如记录日志或抛出异常
+      console.error('Error fetching external data:', error);
+      throw error;
+    }
+  }
+}
+
+
+function sleep(interval) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, interval);
+  });
 }
